47ec8c36881e44a013e627524ac26d19e9c0afb9
Ben Wu <jitsung.wu@gmail.com>
2023-09-26 19:08:45 +0800

    initial vue project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+import TheClick from './components/TheClick.vue';
+</script>
+
+<template>
+  <div>
+    <h1>Hello</h1>
+    <HelloWorld msg="hi"/>
+    <TheClick :init="0" :count="0" />
+  </div>
+</template>
---
+++ src/components/TheClick.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+const props = defineProps(["init", "count"]);
+const emit = defineEmits(["clicked"]);
+const state = reactive({ count: props.init });
+function increment() {
+  state.count++;
+  emit("clicked");
+  alert(state.count);
+}
+</script>
+<template>
+  <div>
+    {{ state.count }}
+    <button @click="increment">點一下</button>
+  </div>
+</template>
