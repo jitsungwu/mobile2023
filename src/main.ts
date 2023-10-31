3c897578246b4159f4d7ae7b43f9c3f99b4f48a5
Ben Wu <jitsung.wu@gmail.com>
2023-10-31 17:20:43 +0800

    remove unnecessary code for icon

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,18 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 // make sure to also import the coresponding css
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
+// import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 // import '@fortawesome/fontawesome-free/css/all.css' // Ensure your project is capable of handling css files
 
 const vuetify = createVuetify({
-  icons: {
-    defaultSet: 'mdi',
-    aliases,
-    sets: {
-      mdi
-    }
-  },
+  // icons: {
+  //   defaultSet: 'mdi',
+  //   aliases,
+  //   sets: {
+  //     mdi
+  //   }
+  // },
   components,
   directives
 })
